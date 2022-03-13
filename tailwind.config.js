8872c8959480aada9d5a596ad30f44d60712a1e5
MissterMan <putrayogi4545@gmail.com>
2022-03-13 16:07:44 +0700

    boardin tailwind ver

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#FCB415",
+      },
+      backgroundImage: {
+        "hero-bg": "url('../img/hero_1.png')",
+      },
+      fontFamily: {
+        nunito: ["Nunito Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
